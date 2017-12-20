f862afaafa131fa8822b5df2dbe8ef71a089aff2
imbdev <moose@inspiredmommyboss.com>
2017-12-20 15:23:00 +0800

    First Commit - 20 Dec 2017

---
+++ config-wp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+read -p "Insert Client Name: " url
+temp=`echo $url | awk -F '.' '{print $1}'`
+echo $temp
+dbext="_db"
+database=$temp$dbext
+title=`echo $url | awk '{print toupper($0)}'`
+adminpass="P@ssw0rdP@ssw0rd"
+
+dnsExt=".local"
+echo dns=$url$dnsExt 
+echo database=$database 
+echo title=$title 
+echo admin=omenteam 
+echo email=support@omeninteractive.com 
+echo password=$adminpass 
+
+read -p "PLEASE VERIFY CONFIG (y or n): " decision
+
+if [ $decision == "y" ] 
+then
+
+echo dns=$url$dnsExt > config
+echo database=$database >> config
+echo title=$title >> config
+echo admin=omenteam >> config
+echo email=support@omeninteractive.com >> config
+echo password=$adminpass >> config
+
+echo "FINAL CONFIG CONTENT"
+cat ./config
+
+else
+
+echo "RUN SCRIPT AGAIN"
+
+fi
